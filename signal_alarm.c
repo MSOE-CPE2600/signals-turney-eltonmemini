379c6510b97f96224ef17b6f2e4c5bb5717f4cf5
Elton MSOE <eminie@msoe.edu>
2025-11-11 10:41:04 -0600

    forgot to do signal_alarm, but is now done

--- signal_alarm.c
+++ signal_alarm.c
@@ -4,3 +4,27 @@
  * 
  * Brief summary of program:
  */
+
+#include <stdlib.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+void alarm_handler() {
+    printf("Alarm Signal Received\n");
+    exit(1);
+}
+
+
+int main()
+{
+    signal(SIGALRM, alarm_handler);
+    alarm(5);
+    while(1)
+    {
+        printf("Sleeping\n");
+        sleep(1);
+    }
+    return 0;
+}
